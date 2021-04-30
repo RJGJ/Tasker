97f5fa67ce7e24a19c72eae2feaa0d86198aa569
RJGJ <jaictinrj@gmail.com>
2021-04-30 10:59:09 +0800

    Patched

--- Tasker/settings.py
+++ Tasker/settings.py
@@ -9,7 +9,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 INSTALLED_APPS = [
@@ -108,9 +110,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
