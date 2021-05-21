7d60cddd9aa752c26e278fcc6e3b96c19357c54c
RJGJ <jaictinrj@gmail.com>
2021-05-21 08:42:19 +0800

    Fixed pylance typing error

--- Tasker/urls.py
+++ Tasker/urls.py
@@ -1,15 +1,15 @@
+from typing import List
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls.resolvers import URLPattern, URLResolver
 
-urlpatterns: list = [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('tasker.urls')),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
