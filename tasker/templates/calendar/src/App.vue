3146ab05d1778053b4dff0c661a442c3ba68200e
RJGJ <jaictinrj@gmail.com>
2021-05-23 16:26:56 +0800

    MVP for calendar

--- tasker/templates/calendar/src/App.vue
+++ tasker/templates/calendar/src/App.vue
@@ -1,53 +1,79 @@
 <template>
   <div id="app">
-    <vc-calendar is-expanded :attributes="attributes" />
+    <vc-calendar is-expanded :attributes="computedTasks" />
   </div>
 </template>
 
 <script>
+import Cookies from "js-cookie";
+import axios from "axios";
+
 export default {
   name: "App",
 
   data() {
     return {
-      departmentID: 0,
       currentDate: new Date(),
-      // Attributes are supplied as an array
-      attributes: [
-        {
-          key: "today",
-          dot: "red",
-          dates: [
-            new Date(2021, 4, 1),
-            new Date(2021, 4, 10),
-            new Date(2021, 4, 22),
-          ],
-          popover: {
-            label: "this is an example popoverrrr",
-          },
-        },
-        {
-          key: "not today",
-          dot: "green",
-          dates: [new Date(2021, 4, 22)],
-          popover: {
-            label: "<3 <3",
-          },
-        },
-      ],
+      tasks: [],
     };
   },
 
-  methods: {
-    getCurrentDate() {
-      return this.currentDate;
+  computed: {
+    computedTasks: function() {
+      let computedVal = [];
+      const colors = [
+        "gray",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "teal",
+        "blue",
+        "indigo",
+        "purple",
+        "pink",
+      ];
+      console.log(this.tasks);
+      for (var idx in this.tasks) {
+        let task = this.tasks[idx];
+        computedVal.push({
+          key: task.pk,
+          dot: colors[(colors.length * Math.random()) | 0],
+          dates: {
+            start: new Date(task.fields.created_on),
+            end: new Date(task.fields.due_on),
+          },
+          popover: {
+            label: `${task.fields.name}: ${task.fields.description}`,
+          },
+        });
+      }
+      return computedVal;
     },
+  },
 
-    getData() {},
+  methods: {
+    getData() {
+      const departmentId = Cookies.get("department_id");
+      axios
+        .get(`http://localhost:8000/api/get-tasks/${departmentId}/`)
+        .then((response) => {
+          const tasks = response.data;
+          this.tasks = tasks;
+        })
+        .catch((err) => console.log(err));
+    },
   },
 
   mounted() {
     this.getData();
+
+    if (window.webpackHotUpdate) {
+      console.log("In Dev Mode.");
+
+      // set temporary cookie
+      Cookies.set("department_id", 1, { expires: 1, path: "" });
+    }
   },
 };
 </script>
